c78424a2a06cc199577a722a9891d043e1c8e70a
Vejay Drepaul <vejayddrepaul@gmail.com>
2022-12-28 23:50:24 -0500

    All download options are working

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO.Packaging;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,7 +18,10 @@ using YoutubeExplode;
 using YoutubeExplode.Videos.Streams;
 using FFMpegCore;
 using System.Threading;
-//https://www.youtube.com/watch?v=afIJop8PriY
+using System.Drawing;
+using System.Windows.Media.Animation;
+using AngleSharp.Text;
+//  https://www.youtube.com/watch?v=YgZjL1Go4uE&list=RDGMEMCMFH2exzjBeE_zAHHJOdxg&start_radio=1&rv=bwW7ni0aTOE
 namespace YouTube_Downloader
 {
     /// <summary>
@@ -30,40 +34,72 @@ namespace YouTube_Downloader
             InitializeComponent();
         }
 
-        private async void download_button_Click(object sender, RoutedEventArgs e)
+        private static async void DownloadAudio(string url)
         {
-            string link = url.Text;
             var youtube = new YoutubeClient();
-            var video = await youtube.Videos.GetAsync(link);
-            string title = video.Title;
+            var video = await youtube.Videos.GetAsync(url);
+            string video_title = video.Title;
 
-            string inputStream = "C:\\Users\\drepa\\Projects\\YouTube-Downloader\\bin\\Debug\\net7.0-windows\\Rack Me, Rack Me.mp4";
-            string auioFile = "C:\\Users\\drepa\\Projects\\YouTube-Downloader\\bin\\Debug\\net7.0-windows\\Rack Me, Rack Me.webm";
-            string outputStream = "C:\\Users\\drepa\\Projects\\YouTube-Downloader\\test\\new.mp4";
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
+            var streamInfo = streamManifest.GetAudioOnlyStreams()
+                .Where(s => s.Container == Container.WebM)
+                .GetWithHighestBitrate();
+            var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+            await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{video_title}.{streamInfo.Container}");
+        }
 
+        private async void DownloadVideoAndAudio(object sender, string url)
+        {
+            DownloadAudio(url);
 
+            var youtube = new YoutubeClient();
+            var video = await youtube.Videos.GetAsync(url);
+            string video_title = video.Title;
+
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
+            var streamInfo = streamManifest
+                .GetVideoOnlyStreams()
+                .Where(s => s.Container == Container.Mp4)
+                .GetWithHighestVideoQuality();
+            var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+            await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{video_title}.{streamInfo.Container}");
+
+            FFMpeg.ReplaceAudio($"{video_title}.{streamInfo.Container}", $"{video_title}.webm", "conv.mp4");
+
+            if (both_radiobutton.IsChecked != true)
+            {
+                File.Delete($"{video_title}.webm");
+                File.Delete($"{video_title}.mp4");
+            }
+
+            MessageBox.Show("Download Complete");
+        }
+
+        private void download_button_Click(object sender, RoutedEventArgs e)
+        {
             if (audio_radiobutton.IsChecked == true)
             {
-                var streamManifest = await youtube.Videos.Streams.GetManifestAsync(link);
-                var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
-                var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
-                await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{title}.{streamInfo.Container}");
+                DownloadAudio(url.Text);
                 MessageBox.Show("Download Complete");
             }
             else if (video_radiobuttton.IsChecked == true)
             {
-                var streamManifest = await youtube.Videos.Streams.GetManifestAsync(link);
-                var streamInfo = streamManifest
-                    .GetVideoOnlyStreams()
-                    .Where(s => s.Container == Container.Mp4)
-                    .GetWithHighestVideoQuality();
-                var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
-                await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{title}.{streamInfo.Container}");
-
-                FFMpeg.ReplaceAudio(inputStream, auioFile, outputStream);
-
-                MessageBox.Show("Download Complete");
+                DownloadVideoAndAudio(true, url.Text);
             }
+            else if (both_radiobutton.IsChecked == true)
+            {
+                DownloadVideoAndAudio(false, url.Text);
+            }
+        }
+
+        private void path_button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.SaveFileDialog();
+            dialog.FileName = "Document"; // Default file name
+            dialog.DefaultExt = ".mp3"; // Default file extension
+            dialog.Filter = "MP3 Files (.mp3)|*.mp3"; // Filter files by extension
+
+            bool? result = dialog.ShowDialog();
         }
     }
 }
